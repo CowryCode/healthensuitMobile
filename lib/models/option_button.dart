b0379b914920e534190d66b5b08d3a33fba6c965
Ifeanyi Odenigbo <33759744+Ifeanyi10@users.noreply.github.com>
2022-07-01 01:09:53 -0300

    Added another sample comment

--- lib/models/option_button.dart
+++ lib/models/option_button.dart
@@ -8,7 +8,7 @@ class OptionButton extends StatelessWidget {
   final Function? buttonEvent;
 
   const OptionButton({Key? key, required this.text, required this.icon, required this.width, this.buttonEvent}) : super(key: key);
-//This is sample comment by ifeanyi.
+//This is a second sample comment today.
   @override
   Widget build(BuildContext context) {
     final ButtonStyle textButtonStyle = TextButton.styleFrom(
